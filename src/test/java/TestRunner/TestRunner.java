2f9a5d0a6dc052c273ccfe4b6500b13dadd38e98
TarasYan <wildmonach@gmail.com>
2026-03-16 12:17:47 +0200

    * Updated Java to 25 * Updated libraries in pom file

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -20,7 +20,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TestRunner {
-    public static final String BASIC_URL = "https://trs.test.abt.team/4184ultru/admin.php?dispatch=addons.manage";
+    public static final String BASIC_URL = "https://trs.test.abt.team/4201ultru/admin.php?dispatch=addons.manage";
     private SoftAssert softAssert;
 
     @BeforeMethod
